1802e6823cb8677408f31a790ccdfc97a053bca7
edo <71205499+edoricardo@users.noreply.github.com>
2023-03-13 16:03:40 +0700

    Update drone_qr_capture.py

--- drone_qr_capture.py
+++ drone_qr_capture.py
@@ -4,23 +4,17 @@ import pyzbar.pyzbar as pyzbar
 
 tello = Tello()
 tello.connect()
-print(tello.get_battery())
+#print(tello.get_battery())
 
 tello.streamon()
 
 myData = "null"
 
-def get_qr():
+while True:
     img = tello.get_frame_read().frame
+    img = cv2.resize(img, (360, 240))
     for barcode in pyzbar(img):
         myData = barcode.data.pyzbar('utf-8')
         print(myData)
-    
-    return
-
-while True:
-    get_qr()
-    image = tello.get_frame_read().frame
-    image = cv2.resize(image, (360, 240))
-    cv2.imshow("Image", image)
+    cv2.imshow("Image", img)
     cv2.waitKey(1)
